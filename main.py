3e04fed59688f3350b4726e49935c1506be93aee
jk-aero <92267875+jk-aero@users.noreply.github.com>
2024-07-03 12:26:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
-response = requests.get("https://api.urbandictionary.com/v0/random")
-api_data=response.json()['list'][3]
-print(api_data)
+
+def randomword():
+    response = requests.get("https://api.urbandictionary.com/v0/random")
+    api_data=response.json()['list'][3]
+    print(api_data)
+    word_defenition=api_data['definition']
+    word=api_data['word']
+    print('\n'+word+'\n')
+    print(word_defenition+'\n')
+
+def wordData(word):
+    response = requests.get(f"http://api.urbandictionary.com/v0/define?term={word}")
+    api_data=response.json()
+    for defenition in (api_data)['list']:
+        print(defenition['definition'])
+
 
